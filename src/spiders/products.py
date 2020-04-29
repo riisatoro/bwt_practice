3a18e82371d0ee65d4eb2b92c7f998b880209b1b
Rissap <rinej.sapset@gmail.com>
2020-04-29 15:31:58 +0300

    dev: non-recursive reviews parsing

--- src/spiders/products.py
+++ src/spiders/products.py
@@ -1,45 +1,97 @@
 import scrapy
 import json
 
+from scrapy.selector import Selector
+
 
 class Products(scrapy.Spider):
-	"""docs"""
-	name = "products"
-	start_urls = [
-		'https://www.tesco.com/groceries/en-GB/shop/household/kitchen-roll-and-tissues/all'
-	]
+    """docs"""
+    name = "products"
+    start_urls = [
+        'https://www.tesco.com/groceries/en-GB/shop/household/kitchen-roll-and-tissues/all'
+    ]
 
-	def parse(self, response):
-		products = response.css('a.sc-cSHVUG::attr(href)').getall()
-		for url in products:
-			yield scrapy.Request(url="https://www.tesco.com"+url, callback=self.split_data)
+    def parse(self, response):
+        #!empty
+        products = response.xpath('//div[@class="product-details--content"]/h3/a/@href').getall()
+        #print("[PRODUCTS]: ", products)
+        for url in products:
+            yield scrapy.Request(url="https://www.tesco.com"+url, callback=self.split_data)
 
-	def split_data(self, response):
-		data = {
-			"image_url": '//img[@class="product-image"]/@src',
-			"prod_title": '//h1[@class="product-details-tile__title"]/text()',
-			"price": '//span[@data-auto="price-value"]/text()',
-		}
+    def split_data(self, response):
+        #dict with all xpath list to simplify editing
+        #data 'in one row'
+        single_data = {
+            "image": '//img[@class="product-image"]/@src',
+            "prod_title": '//h1[@class="product-details-tile__title"]/text()',
+            "price": '//span[@data-auto="price-value"]/text()',
+        }
 
-		product = {}
-		product["prod_URL"] = response.url
-		product["prod_ID"] = int(response.url.split("/")[-1])
-		product["description"] = '. '\
-			.join(response\
-				.xpath('//li[@class="product-info-block__content-item"]/text()')\
-				.getall())
+        #make list of xpath to combine description blocks
+        #xpath for lists and multiple divs
+        mul_data = {
+            "description": '//div[@id="product-description"]/ul/li/text()',
+            "address": '//div[@id="manufacturer-address"]/ul[@class="product-info-block__content"]/li/text()',
+            "return": '//div[@id="return-address"]/ul/li/text()',
+            "net_contents": '//div[@id="net-contents"]/p/text()',
+        }
 
-		for key in data.keys():
-			product[key] = response.xpath(data[key]).get()
+        rev_data = '//p[@class="reviews-list__show-more"]/a/@href'
 
-		yield self.save_data(product)
+        bought_next_data = {
+            "URL": '',
+            "title": '',
+            "image": '',
+            "price": '',
+        }
 
-	def save_data(self, item):
-		with open('parsed/data.json', 'r') as file:
-			data = json.load(file)
-			data['items'].append(item)
-		with open('parsed/data.json', 'w') as file:
-			json.dump(data, file, indent=4)
-		
+        product = {}
+        review = {}
+        bought_next = {}
 
-	
+        product["URL"] = response.url
+        product["ID"] = int(response.url.split("/")[-1])
+
+        for key in single_data.keys():
+            product[key] = response.xpath(single_data[key]).get()
+
+        for key in mul_data.keys():
+            product[key] = ' '.join(response.xpath(mul_data[key]).getall())
+
+        try:
+            product["review"] = self.split_review(response)
+        except Exception as e:
+            print("[EXCEPTION]: ", e)
+
+        yield self.save_data(product)
+
+    def split_review(self, response):
+        review_data = '//article[@class="review"]'
+        
+        review_css = {
+            "title": 'h3.review__summary::text',
+            "stars": 'span.czgxkL::text',
+            "author": 'span.review-author__nickname::text',
+            "date": 'span.review-author__submission-time::text',
+            "text": 'p.review__text::text'
+        }
+        
+        #get all reviews on the page
+        result = []
+        reviews = response.xpath(review_data)
+        for rev in reviews:
+            tmp = {}
+            for key in review_css.keys():
+                tmp[key] = rev.css(review_css[key]).get()
+                if key == "stars":
+                    tmp[key] = int(tmp[key][0])
+            result.append(tmp)
+
+        return result
+
+    def save_data(self, item):
+        with open('parsed/data.json', 'r') as file:
+            data = json.load(file)
+            data['items'].append(item)
+        with open('parsed/data.json', 'w') as file:
+            json.dump(data, file, indent=4)
